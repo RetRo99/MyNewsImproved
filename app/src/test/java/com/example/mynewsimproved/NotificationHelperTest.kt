b55dc1791c345aee492f0c1cd94883e77a8dfd14
Rok Retar <retar.rok@gmail.com>
2020-04-28 15:09:31 +0200

    reverting

---
+++ app/src/test/java/com/example/mynewsimproved/NotificationHelperTest.kt
@@ -0,0 +1,61 @@
+package com.example.mynewsimproved.ui.notification.helper
+
+
+import androidx.test.core.app.ApplicationProvider
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import org.hamcrest.CoreMatchers.`is`
+import org.hamcrest.MatcherAssert.assertThat
+import org.junit.Before
+import org.junit.Test
+import org.junit.runner.RunWith
+import java.text.SimpleDateFormat
+import java.util.*
+
+@RunWith(AndroidJUnit4::class)
+class NotificationHelperTest {
+
+    private lateinit var helper: NotificationHelper
+
+    @Before
+    fun setUp() {
+        helper =
+            NotificationHelper(
+                ApplicationProvider.getApplicationContext()
+            )
+    }
+
+    @Test
+    fun getQuery() {
+        helper.query = "query"
+        assertThat(helper.query, `is`("query"))
+    }
+
+    @Test
+    fun getSections() {
+        helper.sections = "sections"
+        assertThat(helper.sections, `is`("sections"))
+    }
+
+    @Test
+    fun getSwitchState() {
+        helper.switchState = true
+        assertThat(helper.switchState, `is`(true))
+    }
+
+    @Test
+    fun getSearchParams() {
+        helper.query = "query"
+        helper.sections = "sections"
+        helper.switchState = true
+        val params = helper.getSearchParams()
+        val todayString = SimpleDateFormat("yyyyMMdd", Locale.FRANCE).run {
+            format(Date())
+        }
+
+        assertThat(params.sections, `is`(helper.sections))
+        assertThat(helper.query, `is`(helper.query))
+        assertThat(params.startDate, `is`(todayString))
+
+    }
+
+}
